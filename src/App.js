8b2adeece3237b7843f0a7c44cc246b4beb6f5df
Leo Gisselberg <leo.gisselberg@voiapp.io>
2019-12-05 11:36:44 +0100

    New icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { Select } from 'grommet'
+import { Text } from 'grommet'
 
+const locations = ['Stockholm']
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+`
+
+const LocationWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
 function App() {
+  const [location, setLocation] = useState('')
+
+  const handleChange = (city) => {
+    setLocation(city)
+    // Make api request
+  }
+
   return (
-    <div>
-     <h1>Weather app</h1>
-    </div>
+    <Container>
+      <LocationWrapper>
+        <Text>Coose location</Text>
+        <Select
+          options={locations}
+          value={location}
+          onChange={({ value }) => handleChange(value)}
+        />
+      </LocationWrapper>
+      
+    </Container>
   )
 }
 
---
+++ src/api.js
@@ -0,0 +1,4 @@
+import Axios from 'axios'
+
+const BASE_URL = 'https://api.darksky.net/forecast'
+const enpoint = '/[key]/[latitude],[longitude]'
